6be4bdb3efadcfc5db3fb8e256feb06654bbe179
Irham M Fadhil <irham.mfadhil@gmail.com>
2019-05-18 12:57:56 +0700

    update view login

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
+app.get('/login', function(req, res) {
+    res.render('pages/login');
+});
+
 app.listen(4000);
 console.log('4000 is the magic port');
