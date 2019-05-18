425190d06981b298f5134ffe140c5bb9cd3831b3
Irham M Fadhil <irham.mfadhil@gmail.com>
2019-05-18 19:00:14 +0000

    update list username

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var request = require("request")
 app.set('view engine', 'ejs');
 
 // index page 
@@ -17,6 +17,30 @@ app.get('/register', function(req, res) {
 
 app.get('/gate', function(req, res) {
     res.render('pages/gate');
+});
+
+app.get('/users', function(req, res) {
+	var url = "https://pbkk.azurewebsites.net/users"
+	var posts = []
+	request({
+	    url: url,
+	    json: true
+	}, function (error, response, body) {
+
+	    if (!error && response.statusCode === 200) {
+	    	//console.log(body.users)
+	        /*
+	        body.users.forEach((item) => {
+	        	Object.entries(item).forEach(([key, val]) => {
+	        		console.log(`key-${key}-val-${JSON.stringify(val)}`);
+	        	});
+	        });*/
+	        posts = body.users;
+	        console.log(posts);
+	        res.render('pages/users', {posts: posts})
+	        //console.log(body) // Print the json response
+	    }
+	})
 });
 
 app.listen(4000);
